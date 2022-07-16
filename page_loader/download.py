1df0b1247dae139627c756b8b1ab41c7a6e1f7a4
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-07-16 18:55:21 +0300

    fix logic

--- page_loader/download.py
+++ page_loader/download.py
@@ -5,6 +5,31 @@ import shutil
 from bs4 import BeautifulSoup
 
 
+def download(url_string, output_path):
+    if not output_path:
+        output_path = os.getcwd()
+    response = requests.get(url_string)
+    response.raise_for_status()
+    name_of_output_file = output_path + '/' + make_name(url_string, '.html')
+    name_of_output_dir = output_path + '/' + make_name(url_string, '_files')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    change_images = make_change(soup, name_of_output_dir, url_string)
+    output_html = make_output_html(soup, change_images)
+    make_dir_and_img(name_of_output_dir, change_images)
+    with open(name_of_output_file, 'w') as out_file:
+        out_file.write(output_html)
+    return name_of_output_file
+
+
+def make_change(soup, name_of_output_dir, url_string):
+    domain_regex = r"http\w{0,}:\/\/\S{1,}?\/"
+    domain = re.findall(domain_regex, url_string)[0]
+    img_array = make_download_array(soup, 'img')
+    change_img = {i: [name_of_output_dir + '/' + make_name(domain + i, '.png'),
+                      domain + i] for i in img_array}
+    return change_img
+
+
 def make_name(url_string, extension):
     result_name = ''
     for i in url_string:
@@ -27,80 +52,34 @@ def make_name(url_string, extension):
     return result_name
 
 
-def download(url_string, output_path):
-    if not output_path:
-        output_path = os.getcwd()
-    response = requests.get(url_string)
-    response.raise_for_status()
-    name_of_output_file = make_name(url_string, '.html')
-    result = output_path + '/' + name_of_output_file
-    soup = BeautifulSoup(response.text, 'html.parser')
-    tag = soup.img
-    print(tag)
-    img_link_array = list(soup.find_all('img'))
-    # scipt_link_array = list(soup.find_all('script'))
-    # print(*scipt_link_array, sep='\n')
-    # link_link_array = list(soup.find_all('link'))
-    # print(*link_link_array, sep='\n')
+def make_download_array(soup, tag):
+    img_array = list(soup.find_all(tag))
     regex = r"(?<=src=\")(?!http).{1,}(?=\")"
-    img_array = find_local_file(img_link_array, regex)
-    name_of_output_dir = make_name(url_string, '_files')
-    output_html = make_output_html(soup.prettify(),
-                                   img_array,
-                                   name_of_output_dir)
-    download_img(img_array, url_string, output_path, name_of_output_dir)
-    with open(result, 'w') as out_file:
-        out_file.write(output_html)
-    return result
+    img_array = [re.findall(regex, str(i))[0] for i in img_array
+                 if re.findall(regex, str(i))]
+    return img_array
 
 
-def make_output_html(input_html, img_array, name_of_output_dir):
-    imgs = {i: name_of_output_dir + '/' + get_img_name(i) for i in img_array}
-    output_html = str(input_html)
-    for i in imgs:
-        output_html = output_html.replace(i, imgs[i])
-    return output_html
+def make_output_html(soup, change_images):
+    output_html = str(soup)
+    for i in change_images:
+        output_html = output_html.replace(i, change_images[i][0])
+    return BeautifulSoup(output_html, 'html.parser').prettify()
 
 
-def download_img(img_array, url_string, output_path, name_of_output_dir):
-    img_urls = []
-    domain_regex = r"http\w{0,}:\/\/\S{1,}?\/"
-    domain = re.findall(domain_regex, url_string)[0]
-    for i in img_array:
-        if i[0] == '/':
-            temp = domain + i[1:]
-        else:
-            temp = domain + i
-        img_urls.append(temp)
-
-    make_dir_and_img(name_of_output_dir, img_urls, output_path)
-
-
-def make_dir_and_img(name_of_output_dir, img_urls, output_path):
-    dir_path = output_path + '/' + name_of_output_dir
+def make_dir_and_img(name_of_output_dir, change_images):
     try:
-        os.mkdir(dir_path)
+        os.mkdir(name_of_output_dir)
     except FileExistsError:
-        shutil.rmtree(dir_path)
-    os.mkdir(dir_path)
-    for i in img_urls:
-        response = requests.get(i)
+        shutil.rmtree(name_of_output_dir)
+    os.mkdir(name_of_output_dir)
+    for i in change_images:
+        response = requests.get(change_images[i][1])
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError:
             continue
         response.raise_for_status()
-        img_path = dir_path + '/' + get_img_name(i)
+        img_path = change_images[i][0]
         with open(img_path, 'wb') as out_file:
             out_file.write(response.content)
-
-
-def get_img_name(img_url):
-    temp = img_url.split('/')[-1]
-    return make_name(temp, '.png')
-
-
-def find_local_file(img_array, regex):
-    img_array = [re.findall(regex, str(i))[0] for i in img_array
-                 if re.findall(regex, str(i))]
-    return img_array
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,15 +1,33 @@
 import os
 
 from page_loader import download
-import pathlib, tempfile
+from page_loader.download import make_output_html
+from page_loader.download import make_change
+import pathlib
+import tempfile
+from bs4 import BeautifulSoup
 
 
 def test_download():
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        os.mkdir(tmpdirname + '/ru-hexlet-io-courses_files')
-        file_path = download('https://ru.hexlet.io/courses', tmpdirname)
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        os.mkdir(temp_dir_name + '/ru-hexlet-io-courses_files')
+        file_path = download('https://ru.hexlet.io/courses', temp_dir_name)
         print(file_path)
-        assert file_path == tmpdirname + '/ru-hexlet-io-courses.html'
+        assert file_path == temp_dir_name + '/ru-hexlet-io-courses.html'
         with open(file_path, 'r') as test_file:
             temp1 = test_file.read()
             assert temp1
+
+
+def test_make_output_html():
+    path_file_before = 'tests/fixtures/before_ru-hexlet-io-courses.html'
+    path_file_after = 'tests/fixtures/after_ru-hexlet-io-courses.html'
+    with open(path_file_before, 'r') as before_file, \
+         open(path_file_after, 'r') as after_file:
+        after_soup = BeautifulSoup(after_file.read(), 'html.parser')
+        before_soup = BeautifulSoup(before_file.read(), 'html.parser')
+        change_images = make_change(before_soup, 'ru-hexlet-io-courses_files', 'https://ru.hexlet.io/courses')
+        output_html = make_output_html(before_soup, change_images)
+        assert after_soup.title == before_soup.title
+        assert after_soup.prettify() == output_html
+
